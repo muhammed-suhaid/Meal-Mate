abf33427d5ec142f3feffb88898397131818cc15
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-20 20:16:43 +0530

    Created the project "Meal Mate"

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_mate/categories.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meal Mate',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color.fromARGB(255, 131, 57, 0),
+        ),
+        useMaterial3: true,
+        textTheme: GoogleFonts.latoTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CategoriesScreen(),
+    );
+  }
+}
