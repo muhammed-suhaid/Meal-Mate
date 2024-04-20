ea5059a4236386e4ecfb606ba5da49b70354a2be
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-20 21:52:24 +0530

    Created model, dummy data, Meal screen, Meal item for Displaying dynamic meals

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meal_mate/data/dummy_data.dart';
+import 'package:meal_mate/models/category.dart';
+import 'package:meal_mate/screens/meals.dart';
 import 'package:meal_mate/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectcategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +38,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectcategory(context, category);
+              },
+            )
         ],
       ),
     );
